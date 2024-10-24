d0aff3b7fc05cd7075856d24fcbb0439a362db37
Bruno Tenório <tenoriobn@gmail.com>
2024-10-24 12:37:37 -0300

    Criando provedor de dados genérico para reutilizar em outras páginas

--- src/screens/FAQQuestionScreen/index.js
+++ src/screens/FAQQuestionScreen/index.js
@@ -6,6 +6,7 @@ import { cmsService } from '../../infra/cms/cmsService';
 import { renderNodeRule, StructuredText } from 'react-datocms';
 import { isHeading } from 'datocms-structured-text-utils';
 import CMSProvider from '../../infra/cms/CMSProvider';
+import { pageHOC } from '../../components/wrappers/pageHOC';
 
 export async function getStaticPaths() {
   return {
@@ -46,11 +47,11 @@ export async function getStaticProps({ params, preview }) {
   }
 }
 
-export default function FAQQuestionScreen({ cmsContent }) {
+function FAQQuestionScreen({ cmsContent }) {
   console.log('cmsContent: ', cmsContent.globalContent.globalFooter.description)
 
   return (
-    <CMSProvider cmsContent={cmsContent}>
+    <>
       <Head>
         <title>FAQ - Alura</title>
       </Head>
@@ -100,6 +101,8 @@ export default function FAQQuestionScreen({ cmsContent }) {
       </Box>
 
       <Footer />
-    </CMSProvider>
+    </>
   )
 }
+
+export default pageHOC(FAQQuestionScreen)
